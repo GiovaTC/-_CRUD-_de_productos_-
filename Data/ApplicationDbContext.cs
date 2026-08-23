0ff5f76cec7024295c49aec4b536d4f4b4c36011
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-23 10:31:49 -0500

    commit - ApplicationDbContext .

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,41 @@
-﻿namespace CRUDProductos.Data
+﻿using CRUDProductos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDProductos.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        // TABLA productos.
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.ToTable("Productos");
+
+                entity.HasKey(p => p.Id);
+
+                entity.Property(p => p.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(p => p.Descripcion)
+                    .HasMaxLength(500);
+
+                entity.Property(p => p.Precio)
+                    .HasPrecision(18, 2);
+
+                entity.Property(p => p.Stock)
+                    .IsRequired();
+            });
+        }    
+    }   
 }

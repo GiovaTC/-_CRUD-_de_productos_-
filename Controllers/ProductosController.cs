08fe770445b8c349f8c89b8622a66835553f6b4b
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-24 15:24:56 -0500

    commit - ProductosController / .

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,10 +1,28 @@
 using CRUDProductos.Models;
+using CRUDProductos.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDProductos.Controllers
 {
     public class ProductosController : Controller
     {
+        private readonly ApplicationDbContext _context; 
+
+        public ProductosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PRODUCTOS .
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _context.Productos
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(productos);
+        }   
     }   
 }

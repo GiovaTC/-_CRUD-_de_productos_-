d39d5900d7cde79fc62f36e2defaf4fc4267e08e
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-24 15:43:47 -0500

    commit - appsettings.json .

--- Program.cs
+++ Program.cs
@@ -1,29 +1,58 @@
+using CRUDProductos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// =====================================================
+// Configuración de servicios
+// =====================================================
+
+// MVC
 builder.Services.AddControllersWithViews();
+
+// =====================================================
+// Entity Framework Core + MySQL
+// =====================================================
+
+var connectionString = builder.Configuration
+    .GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// =====================================================
+// Configuración del pipeline HTTP
+// =====================================================
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
+// Archivos estáticos
 app.MapStaticAssets();
+
+// =====================================================
+// Ruta principal
+// =====================================================
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Productos}/{action=Index}/{id?}"
+)
+.WithStaticAssets();    
 
 app.Run();

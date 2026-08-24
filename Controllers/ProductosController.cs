0b3f3a519fed085fcaddd02c210ceff05f86c69f
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-24 15:36:32 -0500

    commit - ProductosController .

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -23,6 +23,130 @@ namespace CRUDProductos.Controllers
                 .ToListAsync();
 
             return View(productos);
-        }   
+        }
+
+        // GET: PRODUCTOS/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var producto = await _context.Productos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+            
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: PRODUCTOS/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Productos.Add(producto);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: PRODUCTOS/Edit/5 .
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var producto = await _context.Productos.FindAsync(id);
+            
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: PRODUCTOS/Edit/5 .
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Producto producto)
+        {
+
+            if (id != producto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExiste(producto.Id))
+                    {
+                        return NotFound();
+                    }
+
+                        throw;
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(producto);
+        }
+
+        // GET: PRODUCTOS/Delete/5  .
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: PRODUCTOS/Delete/5  .
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExiste(int id)
+        {
+            return _context.Productos.Any(e => e.Id == id);
+        }
     }   
 }
